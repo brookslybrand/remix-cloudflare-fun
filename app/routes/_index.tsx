5673f65b5844b8dd4454cde80ee4ec5ce6860638
Brooks Lybrand <brookslybrand@gmail.com>
2024-04-03 15:49:42 -0500

    Ridiculous worker config stuff

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -4,22 +4,45 @@ import {
   type MetaFunction,
 } from "@remix-run/cloudflare";
 import { Form, useLoaderData } from "@remix-run/react";
+import { drizzle } from "drizzle-orm/d1";
+import * as schema from "~/.server/schema";
 
 export async function loader({ context }: LoaderFunctionArgs) {
-  const { MY_KV } = context.cloudflare.env;
+  const { MY_KV, DB } = context.cloudflare.env;
+
+  const db = drizzle(DB, { schema });
+
+  const users = await db.query.users.findMany();
+
   const value = await MY_KV.get("my-key");
-  return json({ value });
+  return json({ value, users });
 }
 
 export async function action({ request, context }: LoaderFunctionArgs) {
   const { MY_KV } = context.cloudflare.env;
   const formData = await request.formData();
-  const value = formData.get("value");
-  if (typeof value !== "string") {
-    throw new Error("Invalid value");
+  const intent = formData.get("intent");
+  if (typeof intent !== "string") {
+    throw new Error("Invalid intent");
   }
-  await MY_KV.put("my-key", value);
-  return json({ value });
+  if (intent === "kv") {
+    const value = formData.get("value");
+    if (typeof value !== "string") {
+      throw new Error("Invalid value");
+    }
+    await MY_KV.put("my-key", value);
+    return json({ value });
+  } else if (intent === "add_user") {
+    const { DB } = context.cloudflare.env;
+    const db = drizzle(DB, { schema });
+    const name = formData.get("name");
+    const email = formData.get("email");
+    if (typeof name !== "string" || typeof email !== "string") {
+      throw new Error("Invalid name or email");
+    }
+    const user = await db.insert(schema.users).values({ email, name });
+    return json({ user });
+  }
 }
 
 export const meta: MetaFunction = () => {
@@ -33,7 +56,7 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  const { value } = useLoaderData<typeof loader>();
+  const { value, users } = useLoaderData<typeof loader>();
 
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
@@ -41,8 +64,24 @@ export default function Index() {
       <p>The value is {value ?? "not there :("}</p>
 
       <Form method="post">
+        <input type="hidden" name="intent" value="kv" />
         <input type="text" name="value" />
         <button type="submit">Update the value</button>
+      </Form>
+
+      <h2>Users</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} ({user.email})
+          </li>
+        ))}
+      </ul>
+      <Form method="post">
+        <input type="hidden" name="intent" value="add_user" />
+        <input type="text" name="name" placeholder="Name" />
+        <input type="email" name="email" placeholder="Email" />
+        <button type="submit">Add User</button>
       </Form>
     </div>
   );
