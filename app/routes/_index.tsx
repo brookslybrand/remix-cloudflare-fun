8da49cf02a9c24a7a18b8a3acf48da486b148ae7
Brooks Lybrand <brookslybrand@gmail.com>
2024-04-03 10:08:34 -0500

    Add a kv binding

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,26 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
+import {
+  json,
+  type LoaderFunctionArgs,
+  type MetaFunction,
+} from "@remix-run/cloudflare";
+import { Form, useLoaderData } from "@remix-run/react";
+
+export async function loader({ context }: LoaderFunctionArgs) {
+  const { MY_KV } = context.cloudflare.env;
+  const value = await MY_KV.get("my-key");
+  return json({ value });
+}
+
+export async function action({ request, context }: LoaderFunctionArgs) {
+  const { MY_KV } = context.cloudflare.env;
+  const formData = await request.formData();
+  const value = formData.get("value");
+  if (typeof value !== "string") {
+    throw new Error("Invalid value");
+  }
+  await MY_KV.put("my-key", value);
+  return json({ value });
+}
 
 export const meta: MetaFunction = () => {
   return [
@@ -11,25 +33,17 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const { value } = useLoaderData<typeof loader>();
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix (with Vite and Cloudflare)</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://developers.cloudflare.com/pages/framework-guides/deploy-a-remix-site/"
-            rel="noreferrer"
-          >
-            Cloudflare Pages Docs - Remix guide
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <p>The value is {value ?? "not there :("}</p>
+
+      <Form method="post">
+        <input type="text" name="value" />
+        <button type="submit">Update the value</button>
+      </Form>
     </div>
   );
 }
